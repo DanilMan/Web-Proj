18c332072585546f9801301fed16256ea1569472
Daniel Ruderman <rudermandaniel@gmail.com>
2022-07-13 22:47:15 -0700

    +(optimized blur, changed Block element to class)

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Block.css";
 
 // Probably have to refactor this to a class in order to get a state to
 // handle the image for mouse events
-function Block({ inText, Dest, IMG, ALT, topFunction }) {
-  return (
-    <Link
-      onClick={topFunction}
-      onMouseEnter={mEnter}
-      onMouseLeave={mLeft}
-      className="box"
-      to={Dest}
-    >
-      <img className="b-img" src={IMG} alt={ALT} />
-      <h4 className="title">{inText}</h4>
-    </Link>
-  );
-}
+class Block extends Component {
+  // inText, Dest, IMG, ALT, topFunction
+  state = {
+    img_src: this.props.IMG,
+  };
+  constructor(props) {
+    super(props);
+    this.mEnter = this.mEnter.bind(this);
+    this.mLeave = this.mLeave.bind(this);
+  }
 
-function mEnter() {
-  //console.log("Enter");
-}
+  mEnter() {
+    //console.log("Enter");
+    let img = this.props.IMG;
+    img = img.slice(0, -4);
+    img += "-Blur.png";
+    this.setState({
+      img_src: img,
+    });
+  }
 
-function mLeft() {
-  //console.log("left");
+  mLeave() {
+    //console.log("left");
+    this.setState({
+      img_src: this.props.IMG,
+    });
+  }
+
+  render() {
+    return (
+      <Link
+        onClick={this.props.topFunction}
+        onMouseEnter={this.mEnter}
+        onMouseLeave={this.mLeave}
+        className="box"
+        to={this.props.Dest}
+      >
+        <img className="b-img" src={this.state.img_src} alt={this.props.ALT} />
+        <h4 className="title">{this.props.inText}</h4>
+      </Link>
+    );
+  }
 }
 
 export default Block;
